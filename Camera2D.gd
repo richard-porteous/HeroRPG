c601c30fb230e3b08a51ea73316012d24c3b17c8
Richard Porteous <rich.porteous@gmail.com>
2022-05-06 21:21:12 -0600

    HeroRPG - session4 camera follow

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,9 +1,11 @@
 extends Camera2D
 onready var player = get_node("/root/Node2D/Player")
 
-func _ready():
-	pass
+#func _ready():
+#	position.y = player.position.y
+	
 
 func _physics_process(_delta):
-	position = player.position
+	position.x = player.position.x
+	print (position)
 		
