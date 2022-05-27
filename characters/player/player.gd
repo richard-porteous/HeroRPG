6aed691c00e0bfa5db5172ba4085b442a1ba1859
Richard Porteous <rich.porteous@gmail.com>
2022-05-27 13:48:02 -0600

    temporary

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -6,6 +6,7 @@ onready var _animated_sprite = $AnimatedSprite
 var last_mouse_pos = null
 var velocity:Vector3 = Vector3.ZERO
 var camera_angle = 90
+var flip = true
 
 
 #var flip
@@ -23,19 +24,32 @@ func get_input():
 	if Input.is_action_pressed("run"):
 		multiplier = 1.5
 	if Input.is_action_just_pressed("flip"):
-		if camera_angle > 0:
-			camera_angle = -90
-		else:
-			camera_angle = 90
-		$CameraHub.rotate_y(deg2rad(camera_angle))
-		$AnimatedSprite.rotate_y(deg2rad(camera_angle))
+		flip != flip
+		
+#		if camera_angle > 0:
+#			camera_angle = -90
+#		else:
+#			camera_angle = 90
+#		$CameraHub.rotate_y(deg2rad(camera_angle))
+#		$AnimatedSprite.rotate_y(deg2rad(camera_angle))
 
 #func _input(event):
 #	if event is InputEventMouseButton:
 #		last_mouse_pos = get_global_mouse_position() #event.position #get_viewport().get_mouse_position()
 
-func _physics_process(delta):
+func _process(delta):
+#	if flip == true  && $CameraGimbal.rotation_degrees.y < camera_angle:
+#		var la = lerp(0, camera_angle, delta)
+#		$CameraGimbal.rotate_y($CameraGimbal.rotation_degrees.y-la)
+#	elif flip == false  && $CameraGimbal.rotation_degrees.y > 0:
+#		var la = lerp(camera_angle, 0, delta)
+#		$CameraGimbal.rotate_y($CameraGimbal.rotation_degrees.y+la)
 
+	var camera_pos = $CameraGimbal/InnerGimbal/Camera.global_transform.origin
+	camera_pos.y = 0
+	$AnimatedSprite.look_at(camera_pos, Vector3(0, 1, 0))
+	
+func _physics_process(delta):
 	velocity = Vector3.ZERO
 	multiplier = 1
 
@@ -60,9 +74,9 @@ func _physics_process(delta):
 	else:
 		if velocity.x < 0:
 			anim = "left_right"
-			_animated_sprite.flip_h = true
 		elif velocity.x > 0:
 			anim = "left_right"
+			_animated_sprite.flip_h = true
 	
 	if _animated_sprite.animation != anim:
 		_animated_sprite.stop()
